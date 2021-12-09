3e1a844c49d6bafd7f0c7f691841776fd4c1a6e5
Stevensishernandez <3050560340117@ingenieria.usac.edu.gt>
2021-12-09 02:46:56 -0600

    Modelo creado

---
+++ carga.sql
@@ -0,0 +1,52 @@
+use SoulHealer_201800484;
+
+
+Bulk insert temp 
+
+From 'C:\Users\Steven Sis\Desktop\All\Vacas Diciembre 2021\Semi 2\Lab\Practica1\tsunami historical data from 1800 to 2021.csv'
+
+With(
+Firstrow = 3,
+FIELDTERMINATOR = ',' , 
+ROWTERMINATOR = '\n'
+);
+
+TRUNCATE TABLE temp;
+
+
+
+SELECT * FROM temp WHERE Tsunami_Cause_Code IS NULL;
+
+INSERT INTO TsunamiCauseCode(idTsunamiCauseCode,name)
+SELECT DISTINCT Tsunami_Cause_Code,'default' FROM temp WHERE Tsunami_Cause_Code IS NOT NULL;
+
+
+INSERT INTO TsunamiEventValidity(idTsunamiEventValidity,name)
+SELECT DISTINCT Tsunami_Event_Validity,'default' FROM temp WHERE Tsunami_Event_Validity IS NOT NULL;
+
+
+INSERT INTO dbo.Date (Year,Mo,Dy)
+SELECT DISTINCT Year,Mo,Dy FROM temp;
+
+
+INSERT INTO dbo.location (Country,LocationName)
+SELECT DISTINCT Country,Location_Name FROM temp;
+
+TRUNCATE TABLE  Position;
+INSERT INTO dbo.Position (Latitude,Longitude)
+SELECT DISTINCT Latitude,Longitude FROM temp ;
+
+TRUNCATE TABLE Event;
+INSERT INTO dbo.Event (idDate, idLocation, idPosition, MaximumWaterHeight, NumberofRunups,TsunamiMagnitude, TsunamiIntensity, Hr, Mn, Sec, TotalDeaths, TotalMissing, TotalMissingDescription,
+						TotalInjuries,TotalDamage,TotalDamageDescription, TotalHousesDestroyed, TotalHousesDamaged, idTsunamiCauseCode, idTsunamiEventValidity )
+(SELECT D.idDate, L.idLocation, P.idPosition, T.Maximum_Water_Height, T.Number_of_Runups, T.Tsunami_Magnitude, T.Tsunami_Intensity, T.Hr, T.Mn, T.Sec,
+	T.Total_Deaths, T.Total_Missing, T.Total_Missing_Description, T.Total_Injuries, T.Total_Damage, T.Total_Damage_Description, T.Total_Houses_Destroyed, T.Total_Houses_Damaged,
+	T.Tsunami_Cause_Code, T.Tsunami_Event_Validity
+	FROM Date D, Location L, temp T, Position P 
+WHERE ( (T.Mo = D.Mo OR  (T.Mo IS NULL AND D.Mo IS NULL)) and (T.Dy = D.Dy OR (T.Dy IS NULL AND D.Dy IS NULL) ) AND T.Year = D.Year) AND 
+	  ((L.Country = T.Country OR  (L.Country IS NULL AND T.Country IS NULL) ) and (L.LocationName = T.Location_Name OR  (L.LocationName IS NULL AND T.Location_Name IS NULL))) AND
+	  ((P.Latitude = T.Latitude OR  (P.Latitude IS NULL AND T.Latitude IS NULL) ) and (P.Longitude = T.Longitude OR  (P.Longitude IS NULL AND T.Longitude IS NULL))));
+
+SELECT   * FROM Event;
+
+select count(*)
--- main.py
+++ main.py
@@ -53,13 +53,124 @@ for i in cursor:
 # 			)
 #              ''')
 
+# cursor.execute('''
+# 		CREATE TABLE TsunamiCauseCode (
+# 			idTsunamiCauseCode int PRIMARY KEY,
+# 			name varchar(50)
+# 			)
+#              ''')
+
+# cursor.execute('''
+# 		CREATE TABLE TsunamiEventValidity (
+# 			idTsunamiEventValidity int PRIMARY KEY,
+# 			name varchar(50)
+# 			)
+#              ''')
+
+# cursor.execute('''
+# 		CREATE TABLE Date (
+# 			idDate INT IDENTITY(1,1) PRIMARY KEY,
+# 			Mo INT NULL,
+#             Dy INT NULL,
+#             Year INT NULL
+# 			)
+#              ''')
+
+# cursor.execute('''
+# 		CREATE TABLE Location (
+# 			idLocation INT IDENTITY(1,1) PRIMARY KEY,
+# 			Country varchar(50) NULL,
+#             LocationName varchar(50) NULL
+# 			)
+#              ''')
+
+# cursor.execute('''
+# 		CREATE TABLE Position (
+# 			idPosition INT IDENTITY(1,1) PRIMARY KEY,
+# 			Latitude float NULL,
+#             Longitude float NULL
+# 			)
+#              ''')
+
+# cursor.execute('''
+# 		CREATE TABLE Event (
+# 			idEvent INT IDENTITY(1,1) PRIMARY KEY,
+# 			MaximumWaterHeight float NULL,
+#             NumberofRunups INT NULL,
+#             TsunamiMagnitude float NULL,
+#             TsunamiIntensity float NULL,
+#             TotalDeaths float NULL,
+#             TotalMissing float NULL,
+#             TotalMissingDescription float NULL,
+#             TotalInjuries float NULL,
+#             TotalDamage float NULL,
+#             TotalDamageDescription float NULL,
+#             TotalHousesDestroyed float NULL,
+#             TotalHousesDamaged float NULL,
+#             Hr float NULL,
+#             Mn float NULL,
+#             Sec float NULL,
+#             idPosition INT NULL,
+#             idTsunamiCauseCode INT NULL,
+#             idTsunamiEventValidity INT NULL,
+#             idDate INT NULL,
+#             idLocation INT NULL,
+#             FOREIGN KEY (idPosition) REFERENCES Position(idPosition),
+#             FOREIGN KEY (idTsunamiCauseCode) REFERENCES TsunamiCauseCode(idTsunamiCauseCode),
+#             FOREIGN KEY (idTsunamiEventValidity) REFERENCES TsunamiEventValidity(idTsunamiEventValidity),
+#             FOREIGN KEY (idDate) REFERENCES Date(idDate),
+#             FOREIGN KEY (idLocation) REFERENCES Location(idLocation)
+            
+# 			)
+#              ''')
+
 #Example Carga masiva
-s = ''' Bulk insert temp From 'C:\Users\Steven Sis\Desktop\All\Vacas Diciembre 2021\Semi 2\Lab\Practica1\\tsunami historical data from 1800 to 2021.csv' With( Firstrow = 3,        FIELDTERMINATOR = ',' ,  ROWTERMINATOR = '\\n's  ); '''
+# s = ''' Bulk insert temp From 
+#         'C:\\Users\\Steven Sis\\Desktop\\All\\Vacas Diciembre 2021\\Semi 2\\Lab\\Practica1\\tsunami historical data from 1800 to 2021.csv' 
+#         With( Firstrow = 3,        
+#         FIELDTERMINATOR = ',' ,  
+#         ROWTERMINATOR = '\\n'  ); '''
+# cursor.execute(s)
+
+#Example llenar tablas
+# cursor.execute('''
+# 		INSERT INTO TsunamiCauseCode(idTsunamiCauseCode,name)
+#         SELECT DISTINCT Tsunami_Cause_Code,'default' FROM temp WHERE Tsunami_Cause_Code IS NOT NULL;
+#              ''')
+
+# cursor.execute('''
+# 		INSERT INTO TsunamiEventValidity(idTsunamiEventValidity,name)
+#         SELECT DISTINCT Tsunami_Event_Validity,'default' FROM temp WHERE Tsunami_Event_Validity IS NOT NULL;
+#              ''')
+
+# cursor.execute('''
+#             INSERT INTO dbo.Date (Year,Mo,Dy)
+#             SELECT DISTINCT Year,Mo,Dy FROM temp;
+#               ''')
+
+# cursor.execute('''
+#             INSERT INTO dbo.location (Country,LocationName)
+#             SELECT DISTINCT Country,Location_Name FROM temp;
+#               ''')
+
+
+
+# cursor.execute('''
+#             INSERT INTO dbo.Position (Latitude,Longitude)
+#             SELECT DISTINCT Latitude,Longitude FROM temp WHERE Latitude IS NOT NULL AND Longitude IS NOT NULL;
+#               ''')
+
 cursor.execute('''
-		CREATE TABLE products2 (
-			product_id int primary key,
-			product_name nvarchar(50),
-			price int
-			)
-             ''')
+            INSERT INTO dbo.Event (idDate, idLocation, idPosition, MaximumWaterHeight, NumberofRunups,TsunamiMagnitude, TsunamiIntensity, Hr, Mn, Sec, TotalDeaths, TotalMissing, TotalMissingDescription,
+			TotalInjuries,TotalDamage,TotalDamageDescription, TotalHousesDestroyed, TotalHousesDamaged, idTsunamiCauseCode, idTsunamiEventValidity )
+            (SELECT D.idDate, L.idLocation, P.idPosition, T.Maximum_Water_Height, T.Number_of_Runups, T.Tsunami_Magnitude, T.Tsunami_Intensity, T.Hr, T.Mn, T.Sec,
+                T.Total_Deaths, T.Total_Missing, T.Total_Missing_Description, T.Total_Injuries, T.Total_Damage, T.Total_Damage_Description, T.Total_Houses_Destroyed, T.Total_Houses_Damaged,
+                T.Tsunami_Cause_Code, T.Tsunami_Event_Validity
+                FROM Date D, Location L, temp T, Position P 
+            WHERE ( (T.Mo = D.Mo OR  (T.Mo IS NULL AND D.Mo IS NULL)) and (T.Dy = D.Dy OR (T.Dy IS NULL AND D.Dy IS NULL) ) AND T.Year = D.Year) AND 
+                ((L.Country = T.Country OR  (L.Country IS NULL AND T.Country IS NULL) ) and (L.LocationName = T.Location_Name OR  (L.LocationName IS NULL AND T.Location_Name IS NULL))) AND
+                ((P.Latitude = T.Latitude OR  (P.Latitude IS NULL AND T.Latitude IS NULL) ) and (P.Longitude = T.Longitude OR  (P.Longitude IS NULL AND T.Longitude IS NULL))));
+              ''')
+
+
 conn.commit()
