3d2600c69c25ed9ad9d6f73644d62306bc268b58
Steven Sis <3050560340117@ingenieria.usac.edu.gt>
2021-12-10 12:29:24 -0600

    Practice was teminated

--- consultas.sql
+++ consultas.sql
@@ -1,3 +1,20 @@
+-- Consulta 1
+SELECT
+                QUOTENAME(SCHEMA_NAME(sOBJ.schema_id)) + '.' + QUOTENAME(sOBJ.name) AS [TableName]
+                , SUM(sdmvPTNS.row_count) AS [RowCount]
+            FROM
+                sys.objects AS sOBJ
+                INNER JOIN sys.dm_db_partition_stats AS sdmvPTNS
+                        ON sOBJ.object_id = sdmvPTNS.object_id
+            WHERE 
+                sOBJ.type = 'U'
+                AND sOBJ.is_ms_shipped = 0x0
+                AND sdmvPTNS.index_id < 2
+            GROUP BY
+                sOBJ.schema_id
+                , sOBJ.name
+            ORDER BY [TableName]
+
 -- Cosulta 2
 SELECT * FROM (
 	SELECT D.Year, L.Country, ROW_NUMBER() OVER(Partition by D.Year ORDER BY L.Country) AS Row_Number FROM Event E
